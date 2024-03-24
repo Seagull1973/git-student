2fff0a4a89229898e57b7344b91b62fe68ba11be
Seagull1973 <157147502+Seagull1973@users.noreply.github.com>
2024-03-24 12:04:04 +0100

    Add files via upload

---
+++ ZaverecnyProjekt/Evidence/Evidence/Databaze.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencePojisteni
+{
+    internal class Databaze
+    { 
+        private List<Pojistenci> pojistenciList; 
+
+        public Databaze() 
+        {
+            pojistenciList = new List<Pojistenci>();  
+        }
+        public void PridejPojisteneho(string jmeno, string prijmeni, string telefon, int vek)
+        {
+            pojistenciList.Add(new Pojistenci( jmeno, prijmeni, telefon, vek));
+        } 
+        public List<Pojistenci> VypisVsechnyPojistene()
+        {
+            return pojistenciList; 
+        } 
+        public List<Pojistenci>  VyhledejPojisteneho(string jmeno, string prijmeni)
+        {
+            foreach (Pojistenci p in pojistenciList)
+            {
+                if(p.jmeno == jmeno && p.prijmeni == prijmeni)
+                {
+                    return p;
+                }
+            } 
+            return pojistenciList;
+        }
+        
+            
+        
+
+
+
+    }
+}
---
+++ ZaverecnyProjekt/Evidence/Evidence/Pojistenci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencePojisteni
+{
+    internal class Pojistenci
+    {
+        private string jmeno;
+        private string prijmeni;
+        private string telefon;
+        private int vek;
+
+        public Pojistenci(string jmeno, string prijmeni, string telefon, int vek)
+        {
+            this.jmeno = jmeno;
+            this.prijmeni = prijmeni;
+            this.telefon = telefon;
+            this.vek = vek;
+        }
+        public override string ToString()
+        {
+            return jmeno + " " + prijmeni + " " + telefon + " " + vek;
+        }
+
+
+
+
+    }
+}
